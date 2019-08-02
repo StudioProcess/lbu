3873209c5f5ebb01978a4daad37ca103f7a23620
Martin Leopold <m@martinleopold.com>
2019-08-02 11:42:15 +0200

    Added MeshLine (patched for module support)

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { MeshLine, MeshLineMaterial } from './THREE.MeshLine.module.js';
 import * as util from './util.js';
 // import * as data from '../data/random_on_land_000.json';
 
