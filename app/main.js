0173f4a862d85bc97e5cd0a3ff1809d56f139f94
RESL <moritz@process.studio>
2019-08-27 17:25:34 +0200

    Visualization changes

--- app/main.js
+++ app/main.js
@@ -26,7 +26,7 @@ let numberOfPoints;
 var lines = [];
 var resolution = new THREE.Vector2( window.innerWidth, window.innerHeight );
 
-let projectColors = [ '#ce3b43', '#2452c2', '#f2c200', '#b94db3', '#9bcfe4', '#b7b5a8', '#CCCCCC', '#ccf0fd'];
+let projectColors = [ '#ce3b43', '#2452c2', '#f2c200', '#b94db3', '#9bcfe4', '#b7b5a8', '#CCCCCC', '#ccf0fd', '#ff0000', '#00ff00', '#ffff00', '#99000FF', '#FF00F0', '#CCDD00'];
 
 let pointColorIndex = [];
 var Params = function() {
@@ -72,7 +72,6 @@ async function setup() {
 
   for(let i = 1; i <= numberOfPoints; i++) {
     let randColorIndex = getRndInteger(0, projectColors.length-2);
-    pointColorIndex.push(randColorIndex);
   }
 
   for(let u = 0; u < data1.length; u++){ // 321 points
@@ -81,6 +80,8 @@ async function setup() {
 
       let lat = data1[u][o][0];
       let lon = data1[u][o][1];
+
+      console.log("lat: "+data1[u][o][0]+" long: "+data1[u][o][1]);
 
       var cosLat = Math.cos(lat * Math.PI / 180.0);
       var sinLat = Math.sin(lat * Math.PI / 180.0);
@@ -111,10 +112,6 @@ async function setup() {
   controls = new OrbitControls( camera, renderer.domElement );
   camera.position.z = 70;
 
-  // showDots();
-  // volumeConnect();
-  // connectPathLine();
-
   createTravelledPaths();
   showLastPositionAsDots();
   connectToCenter();
@@ -125,35 +122,38 @@ async function setup() {
   // scene.add( directionalLight );
   // scene.add( ambientLight );
 
-  lbu.onData( ( data ) => {
-    // console.log( data );
-    // console.log( Object.keys(data.integrated).length ); // number of current paths in dataset
-    // console.log( data.updated ); // last updated path
-    // console.log( data.integrated[data.updated] ); // stream of points of last updated path
-    // console.log( data.integrated ); // current data
+  // testData();
+}
 
+function testData(){
+  lbu.onData( ( data ) => {
+    console.log( data );
+    console.log( Object.keys(data.integrated).length ); // number of current paths in dataset
+    console.log( data.updated ); // last updated path
+    console.log( data.integrated[data.updated] ); // stream of points of last updated path
+    console.log( data.integrated ); // current data
   });
 
 }
 
+let indexDot = 1;
 function showLastPositionAsDots() {
   lbu.onData( ( data ) => {
-    // console.log( data );
-    // console.log( Object.keys(data.integrated).length ); // number of current paths in dataset
-    // console.log( data.updated ); // last updated path
-    // console.log( data.integrated[data.updated] ); // stream of points of last updated path
-    // console.log( data.integrated ); // current data
-
     for (let key of Object.keys(data.integrated)) {
       let points = data.integrated[key];
       let dotGeo = null;
       let mat = null;
 
-      dotGeo = new THREE.SphereGeometry( objectSize/4, 5, 5 );
+      for (let i = 0; i < points.length-1; i++) {
 
-      for (let i = 0; i < points.length; i++) {
+        if(i == points.length-2) { // last point
+          dotGeo = new THREE.SphereGeometry( objectSize/2, 5, 5 );
+          pointColorIndex.push(indexDot);
+          console.log("indexDot: "+indexDot);
 
-        // console.log ( points.length );
+        } else {
+          dotGeo = new THREE.SphereGeometry( objectSize/10, 5, 5 );
+        }
 
         let lat = points[i];
         let lon = points[i+1];
@@ -172,7 +172,7 @@ function showLastPositionAsDots() {
         mat = new MeshLineMaterial( {
           // map: strokeTexture,
           useMap: params.strokes,
-          color: new THREE.Color( projectColors[0] ),
+          color: new THREE.Color( projectColors[indexDot%projectColors.length] ),
           opacity: params.strokes ? .5 : 1,
           dashArray: params.dashArray,
           dashOffset: params.dashOffset,
@@ -197,54 +197,15 @@ function showLastPositionAsDots() {
         // console.log("mesh: "+mesh);
         i++;
       }
+      indexDot++;
     }
 
   });
 }
 
-function showDots(){
-  let numberDots = 0;
-  for(let i = 0; i < coordinates1.length; i++){
-    // let dotGeo = new THREE.CylinderBufferGeometry( objectSize, objectSize, objectSize/3, 6, 6 );
-    let dotGeo = null;
-    let mat = null;
-    if(i%pointHistory==0 && numberDots<numberOfPoints) {
-      dotGeo = new THREE.SphereGeometry( objectSize/4, 5, 5 );
-      // let randColorIndex = getRndInteger(0, projectColors.length-2);
-      // console.log(pointColorIndex[numberDots]);
-      // mat = new THREE.MeshLambertMaterial({ color: projectColors[pointColorIndex[numberDots]], flatShading: true, transparent: false, opacity: 1.0 });
-
-      mat = new MeshLineMaterial( {
-        // map: strokeTexture,
-        useMap: params.strokes,
-        color: new THREE.Color( projectColors[pointColorIndex[numberDots]] ),
-        opacity: params.strokes ? .5 : 1,
-        dashArray: params.dashArray,
-        dashOffset: params.dashOffset,
-        dashRatio: params.dashRatio,
-        resolution: resolution,
-        sizeAttenuation: params.sizeAttenuation,
-        lineWidth: centerConnectionWidth,
-        near: camera.near,
-        far: camera.far,
-        depthWrite: false,
-        depthTest: !params.strokes,
-        alphaTest: .5,//params.strokes ? .5 : 0,
-        transparent: true,
-        side: THREE.DoubleSide
-    });
-
-
-      let mesh = new THREE.Mesh( dotGeo, mat );
-      mesh.position.set(coordinates1[i].x, coordinates1[i].y, coordinates1[i].z);
-      dots.push( mesh );
-      scene.add( mesh );
-      numberDots++;
-    }
-  }
-}
-
 function createTravelledPaths(){
+
+  let index = 0;
 
   lbu.onData( ( data ) => {
     // console.log( data );
@@ -259,6 +220,7 @@ function createTravelledPaths(){
       let points = data.integrated[key];
       var line = new MeshLine();
       let geometry = new THREE.Geometry();
+      index++;
 
       for (let i = 0; i < points.length; i++) {
 
@@ -284,13 +246,13 @@ function createTravelledPaths(){
         i++;
       }
 
-
+      console.log(index);
       line.setGeometry( geometry );
 
       var splineMat = new MeshLineMaterial( {
         // map: strokeTexture,
         useMap: params.strokes,
-        color: new THREE.Color( projectColors[2] ),
+        color: new THREE.Color( projectColors[index] ),
         opacity: 0.9,//params.strokes ? .5 : 1,
         dashArray: params.dashArray,
         dashOffset: params.dashOffset,
@@ -315,137 +277,6 @@ function createTravelledPaths(){
   });
 }
 
-function connectPathLine(){
-  for(let i = 0; i < numberOfPoints; i++) {
-    let geometry = new THREE.Geometry();
-    for(let u = 0; u < pointHistory; u++) {
-      geometry.vertices.push( new THREE.Vector3(
-        coordinates1[i*pointHistory+u].x,
-        coordinates1[i*pointHistory+u].y,
-        coordinates1[i*pointHistory+u].z
-      ) );
-    }
-    var line = new MeshLine();
-
-    line.setGeometry( geometry );
-
-    // var splineMat = new MeshLineMaterial( {
-    //   color: new THREE.Color( projectColors[pointColorIndex[i]] ),
-    //   opacity: 0.8,
-    //   sizeAttenuation: true,
-    //   lineWidth: pathConnectionWidth,
-    //   depthWrite: true,
-    //   wireframe: false,
-    //   transparent: true,
-    //   side: THREE.DoubleSide,
-    //   dashArray: 2,     // always has to be the double of the line
-    //   dashOffset: -1,    // start the dash at zero
-    //   dashRatio: 0.2
-    // });
-
-    var splineMat = new MeshLineMaterial( {
-      // map: strokeTexture,
-      useMap: params.strokes,
-      color: new THREE.Color( projectColors[pointColorIndex[i]] ),
-      opacity: 0.9,//params.strokes ? .5 : 1,
-      dashArray: params.dashArray,
-      dashOffset: params.dashOffset,
-      dashRatio: params.dashRatio,
-      resolution: resolution,
-      sizeAttenuation: params.sizeAttenuation,
-      lineWidth: params.lineWidth,
-      near: camera.near,
-      far: camera.far,
-      depthWrite: false,
-      depthTest: !params.strokes,
-      alphaTest: params.strokes ? .5 : 0,
-      transparent: true,
-      side: THREE.DoubleSide
-	});
-
-    let mesh = new THREE.Mesh( line.geometry, splineMat );
-    lines.push ( mesh );
-    scene.add( mesh );
-  }
-}
-
-function volumeConnect(){
-
-  // let splineMat = new THREE.MeshLambertMaterial( { color: 0xccf0fd, flatShading: false, wireframe: false, transparent: true, opacity: 0.8 } );
-  var centerX = 0;
-  var centerY = 0;
-  var centerZ = 0;
-
-  for(let i = 0; i < coordinates1.length; i++){
-
-    if(i%pointHistory==0 && i < numberOfPoints*pointHistory){
-      var midpointX = (coordinates1[i].x+centerX)/2;
-      var midpointY = (coordinates1[i].y+centerY)/2;
-      var midpointZ = (coordinates1[i].z+centerZ)/2;
-
-      var geometry = new THREE.Geometry();
-
-      geometry.vertices.push( new THREE.Vector3( coordinates1[i].x, coordinates1[i].y, coordinates1[i].z ) );
-      geometry.vertices.push( new THREE.Vector3( midpointX, midpointY, midpointZ ) );
-      geometry.vertices.push( new THREE.Vector3( centerX, centerY, centerZ ) );
-
-      // splines.push ( spline );
-      // var tubeGeometry = new THREE.TubeBufferGeometry( spline, 4, centerConnectionWidth, 4, false );
-
-      // let mesh = new THREE.Mesh( tubeGeometry, splineMat );
-      // var wireframe = new THREE.Mesh( geometry, wireframeMaterial );
-      // mesh.add( wireframe );
-      // lines.push ( mesh );
-      // scene.add( mesh );
-
-      var line = new MeshLine();
-
-      line.setGeometry( geometry );
-
-      // var centerMat = new MeshLineMaterial( {
-      //   color: new THREE.Color( 0xffffff ),
-      //   opacity: 0.5,
-      //   sizeAttenuation: true,
-      //   lineWidth: centerConnectionWidth,
-      //   depthWrite: true,
-      //   wireframe: false,
-      //   transparent: true,
-      //   side: THREE.DoubleSide,
-      //   dashArray: 2,     // always has to be the double of the line
-      //   dashOffset: -1,    // start the dash at zero
-      //   dashRatio: 0.2
-      // });
-
-      var centerMat = new MeshLineMaterial( {
-        // map: strokeTexture,
-        useMap: params.strokes,
-        color: new THREE.Color( 0xffffff ),
-        opacity: params.strokes ? .5 : 1,
-        dashArray: params.dashArray,
-        dashOffset: params.dashOffset,
-        dashRatio: params.dashRatio,
-        resolution: resolution,
-        sizeAttenuation: params.sizeAttenuation,
-        lineWidth: centerConnectionWidth,
-        near: camera.near,
-        far: camera.far,
-        depthWrite: false,
-        depthTest: !params.strokes,
-        alphaTest: .5,//params.strokes ? .5 : 0,
-        transparent: true,
-        side: THREE.DoubleSide
-    });
-
-      let mesh = new THREE.Mesh( line.geometry, centerMat );
-      lines.push ( mesh );
-      scene.add( mesh );
-
-    }
-  }
-
-  // console.log(scene);
-
-}
 
 function connectToCenter() {
   lbu.onData( ( data ) => {
@@ -477,7 +308,7 @@ function connectToCenter() {
       let center = new THREE.Vector3( 0, 0, 0);
       let point = new THREE.Vector3( x, y, z);
 
-      console.log("from: "+center.x+" "+center.y+" "+center.z+" to: "+point.x+" "+point.y+" "+point.z);
+      // console.log("from: "+center.x+" "+center.y+" "+center.z+" to: "+point.x+" "+point.y+" "+point.z);
 
       geometry.vertices.push( center );
       geometry.vertices.push( point );
@@ -487,7 +318,7 @@ function connectToCenter() {
       var splineMat = new MeshLineMaterial( {
         // map: strokeTexture,
         useMap: params.strokes,
-        color: new THREE.Color( projectColors[0] ),
+        color: new THREE.Color( 0xFFFFFF ),
         opacity: 0.9,//params.strokes ? .5 : 1,
         dashArray: params.dashArray,
         dashOffset: params.dashOffset,
@@ -512,10 +343,6 @@ function connectToCenter() {
   });
 }
 
-function animateRandomPath() {
-  scene.children[getRndInteger(numberOfPoints*2, numberOfPoints*pointHistory)].material.opacity = getRndInteger(1,10)*0.1;
-}
-
 // always returns a random number between min and max (both included)
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -528,31 +355,20 @@ function loop(time) { // eslint-disable-line no-unused-vars
   var speed = Date.now() * 0.00005;
   camera.position.x = Math.cos(speed) * 60;
   camera.position.z = Math.sin(speed) * 60;
-
-  // animateRandomPath();
-
   camera.lookAt(scene.position);
 
   requestAnimationFrame( loop );
 
-  // // animation dots
-  // dots.forEach( function( l, i ) {
-  //   l.radius = 2.0 + i*10;
-  // } );
-
   // animation centerConnection
   lines.forEach( function( l, i ) {
-    if (i <= 27) { // TODO: current number of entries here instead of 27
-      // l.material.uniforms.visibility.value = Math.cos(time/(6000-i));// % 1.0;
-      l.material.uniforms.visibility.value = Math.abs(Math.cos(speed) * 0.6);
-      // console.log( l.material.uniforms.visibility.value = Math.abs(Math.cos(speed) * 0.6) );
-    }
+    // if (i <= 27) { // TODO: current number of entries here instead of 27
+    //   // l.material.uniforms.visibility.value = Math.cos(time/(6000-i));// % 1.0;
+    //   l.material.uniforms.visibility.value = Math.abs(Math.cos(speed) * 0.6);
+    //   // console.log( l.material.uniforms.visibility.value = Math.abs(Math.cos(speed) * 0.6) );
+    // }
   } );
 
   renderer.render( scene, camera );
-
-
-  // console.log( scene.children );
 }
 
 
@@ -661,6 +477,7 @@ function initUpload() {
       if(err.name == 'InvalidCode') { document.getElementById("InvalidCode").style.display = "block"; }
       if(err.name == 'UploadSize') { document.getElementById("UploadSize").style.display = "block"; }
       if(err.name == 'UploadError') { document.getElementById("UploadError").style.display = "block"; }
+      if(err.name == 'MissingCode') { document.getElementById("MissingCode").style.display = "block"; }
       throw err.message;
     });
   });
