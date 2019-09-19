3bdb0b512098fb8143d44bf525c3b337e881401d
Martin Leopold <m@martinleopold.com>
2019-09-19 09:20:16 +0200

    Added robots.txt to build

--- parcel.js
+++ parcel.js
@@ -3,7 +3,7 @@
 // adds a small fix to bundled index.html. See: https://github.com/parcel-bundler/parcel/issues/1401#issuecomment-477869363
 
 const { execSync } = require('child_process');
-const fs = require('fs');
+const fs = require('fs-extra');
 
 // Execute a command
 function exec(cmd, continueOnFailure = false) {
@@ -20,6 +20,9 @@ function exec(cmd, continueOnFailure = false) {
   return result;
 }
 
+// clean build dir
+exec(`rm -rf dist`);
+
 // run parcel
 exec(`npx parcel build index.html`);
 
@@ -28,3 +31,11 @@ const INDEX_FILE = './dist/index.html';
 let index = fs.readFileSync(INDEX_FILE, {encoding:'utf8'});
 index = index.replace('script type="module"', 'script');
 fs.writeFileSync(INDEX_FILE, index);
+
+// copy robots.txt if it's there
+try {
+  fs.copySync('./robots.txt', './dist/robots.txt')
+  console.log('WARNING: Copied robots.txt. REMOVE ROBOTS.TXT IN PRODUCTION TO ALLOW INDEXING!');
+} catch (err) {
+  // nop
+}
